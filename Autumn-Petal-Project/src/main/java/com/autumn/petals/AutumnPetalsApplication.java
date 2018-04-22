4580785f646b6fadf26e2ed49a9b42c98d5c20cb
David Jiang <djiang86@binghamton.edu>
2018-04-22 01:50:05 +0000

    started spriring for backend

---
+++ Autumn-Petal-Project/src/main/java/com/autumn/petals/AutumnPetalsApplication.java
@@ -0,0 +1,12 @@
+package com.autumn.petals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutumnPetalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutumnPetalsApplication.class, args);
+	}
+}
---
+++ Autumn-Petal-Project/src/test/java/com/autumn/petals/AutumnPetalsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.autumn.petals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AutumnPetalsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
